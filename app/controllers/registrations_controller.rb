2ae509c70bcb599ebaa9e95476b8292c056b018f
Clemence <clemencethune@hotmail.com>
2017-10-10 15:00:28 +0000

    Correction bug MAJ mot de passe

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,17 @@ class RegistrationsController < Devise::RegistrationsController
     
     protected
     def update_resource(resource, params)
-        resource.update_without_password(params)
+        if password_required?
+            resource.update_with_password(params)
+        else
+            params.delete(:current_password)
+            resource.update_without_password(params)
+        end
     end
+  
+  private
+  def password_required?
+    (resource.email != params[:user][:email] if params[:user][:email].present?) || params[:user][:password].present?
+  end
     
 end
